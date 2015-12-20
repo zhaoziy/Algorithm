ebf98e43319b58e46acdfe368da36995aa68e1e4
Ziyun Zhao <zhaoziy@hotmail.com>
2015-12-20 21:39:22 +0800

    new

---
+++ ShellSort/ShellSort/ShellSort.cpp
@@ -0,0 +1,34 @@
+// ShellSort.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+class ShellSort {
+public:
+	int* shellSort(int* A, int n) {
+		// write code here
+	}
+
+	void swap(int &a, int &b)
+	{
+		int temp = 0;
+		temp = a;
+		a = b;
+		b = temp;
+	}
+};
+
+int main()
+{
+	int init_Array[13] = { 54,35,48,36,27,12,44,44,8,14,26,17,28 };
+	//int init_Array[4] = { 54,35,48,36 };
+	ShellSort sort;
+	sort.shellSort(init_Array, 13);
+	for (int i = 0; i < 13; ++i)
+	{
+		cout << init_Array[i] << endl;
+	}
+	return 0;
+}
