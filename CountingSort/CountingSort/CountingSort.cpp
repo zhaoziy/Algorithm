2d27edcf68f11399302aeb0ca8d2d0e28d50611f
Ziyun Zhao <zhaoziy@hotmail.com>
2015-12-26 22:22:14 +0800

    new

--- CountingSort/CountingSort/CountingSort.cpp
+++ CountingSort/CountingSort/CountingSort.cpp
@@ -3,27 +3,60 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class CountingSort {
 public:
+	typedef struct  Num
+	{
+		int num;
+		int length;
+	};
 	int* countingSort(int* A, int n) {
 		// write code here
-	}
-
-	void swap(int &a, int &b)
-	{
-		int temp = 0;
-		temp = a;
-		a = b;
-		b = temp;
+		int Max = A[0];
+		int Min = A[0];
+		for (int i = 0; i < n; ++i)
+		{
+			if (A[i] > Max)
+			{
+				Max = A[i];
+			}
+			if (A[i] < Min)
+			{
+				Min = A[i];
+			}
+		}
+		int Length = Max - Min + 1;
+		Num *B = new Num[Length];
+		for (int i = 0; i < Length; ++i)
+		{
+			B[i].num = 0;
+			B[i].length = 0;
+		}
+		for (int i = 0; i < n; ++i)
+		{
+			B[A[i] - Min].num = A[i];
+			B[A[i] - Min].length += 1;
+		}
+		int count = 0;
+		for (int i = 0; i < Length; ++i)
+		{
+			for (int k = 0; k < B[i].length; ++k)
+			{
+				A[count] = B[i].num;
+				count++;
+			}
+		}
+		return A;
 	}
 };
 
 int main()
 {
 	int init_Array[13] = { 54,35,48,36,27,12,44,44,8,14,26,17,28 };
-	//int init_Array[4] = { 54,35,48,36 };
+	//int init_Array[4] = { 4,3,2,1 };
 	CountingSort sort;
 	sort.countingSort(init_Array, 13);
 	for (int i = 0; i < 13; ++i)
---
+++ RadixSort/RadixSort/RadixSort.cpp
@@ -0,0 +1,26 @@
+// RadixSort.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+class RadixSort {
+public:
+	int* radixSort(int* A, int n) {
+		// write code here
+	}
+};
+
+int main()
+{
+	int init_Array[13] = { 54,35,48,36,27,12,44,44,8,14,26,17,28 };
+	//int init_Array[4] = { 4,3,2,1 };
+	RadixSort sort;
+	sort.radixSort(init_Array, 13);
+	for (int i = 0; i < 13; ++i)
+	{
+		cout << init_Array[i] << endl;
+	}
+	return 0;
+}
