9d2092323a959591e8b597fd010561a43ccad8be
Ziyun Zhao <zhaoziy@hotmail.com>
2015-12-29 23:07:43 +0800

    new

--- Subsequence/Subsequence/Subsequence.cpp
+++ Subsequence/Subsequence/Subsequence.cpp
@@ -8,15 +8,50 @@ using namespace std;
 
 class Subsequence {
 public:
-	int shortestSubsequence(vector<int> A, int n) {
+	int shortestSubsequence(vector<int> A, int n)
+	{
 		// write code here
+		int MAX = A[0];
+		int right = 0;
+		for (int i = 0; i < n; ++i)
+		{
+			if (A[i] >= MAX)
+			{
+				MAX = A[i];
+			}
+			else
+			{
+				right = i;
+			}
+		}
 
+		int MIN = A[n - 1];
+		int left = n - 1;
+		for (int i = n - 1; i >= 0; --i)
+		{
+			if (A[i] <= MIN)
+			{
+				MIN = A[i];
+			}
+			else
+			{
+				left = i;
+			}
+		}
+		if (left >= right)
+		{
+			return 0;
+		}
+		else
+		{
+			return right - left + 1;
+		}
 	}
 };
 
 int main()
 {
-	int temp[6] = { 1,4,6,5,9,10 };
+	int temp[6] = { 1,2,3,3,8,9 };
 	vector<int> init_Array;
 	for (int i = 0; i < 6; ++i)
 	{
