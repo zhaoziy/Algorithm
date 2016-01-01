54d19a77b50c9f3c0fc7db7eff8b37c24d46a960
Ziyun Zhao <zhaoziy@hotmail.com>
2016-01-01 23:42:54 +0800

    new

--- TreePrinter/TreePrinter/TreePrinter.cpp
+++ TreePrinter/TreePrinter/TreePrinter.cpp
@@ -3,6 +3,8 @@
 
 #include "stdafx.h"
 #include <vector>
+#include <deque>
+#include <iostream>
 using namespace std;
 
 struct TreeNode {
@@ -18,11 +20,76 @@ class TreePrinter {
 public:
 	vector<vector<int> > printTree(TreeNode* root) {
 		// write code here
+		vector<vector<int>> result;
+		vector<int> temp;
+		vector<TreeNode*>  EndNode;
+		Deque.push_back(root);
+		
+		TreeNode* last = root;
+		TreeNode* nlast = root;
+
+		while (!Deque.empty())
+		{
+			if (Deque.front()->left != NULL)
+			{
+				Deque.push_back(Deque.front()->left);
+				nlast = Deque.front()->left;
+			}
+			if (Deque.front()->right != NULL)
+			{
+				Deque.push_back(Deque.front()->right);
+				nlast = Deque.front()->right;
+			}
+			
+			if (Deque.front() == last)
+			{
+				temp.push_back(Deque.front()->val);
+				result.push_back(temp);
+				temp.clear();
+				last = nlast;
+			}
+			else
+			{
+				temp.push_back(Deque.front()->val);
+			}
+
+			Deque.pop_front();
+		}
+		return result;
 	}
+private:
+	deque<TreeNode *> Deque;
 };
 
 int main()
 {
+	TreeNode Node0(0);
+	TreeNode Node1(1);
+	TreeNode Node2(2);
+	TreeNode Node3(3);
+	TreeNode Node4(4);
+	TreeNode Node5(5);
+	TreeNode Node6(6);
+	TreeNode Node7(7);
+	Node0.left = &Node1;
+	Node0.right = &Node2;
+	Node1.left = &Node3;
+	Node1.right = &Node4;
+	Node2.left = &Node5;
+	Node2.right = &Node6;
+	Node3.left = &Node7;
+
+	TreePrinter treePrinter;
+	vector<vector<int>> Result;
+	Result = treePrinter.printTree(&Node0);
+
+	for (int i = 0; i < Result.size(); ++i)
+	{
+		for (int j = 0; j < Result[i].size(); ++j)
+		{
+			cout << Result[i][j] << " ";
+		}
+		cout << endl;
+	}
 	return 0;
 }
-
