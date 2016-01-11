c47a2d89d3160088880ac801ca94e1993728fa39
Ziyun Zhao <zhaoziy@hotmail.com>
2016-01-11 11:28:02 +0800

    new

--- listDivide/listDivide/listDivide.cpp
+++ listDivide/listDivide/listDivide.cpp
@@ -15,11 +15,54 @@ class Divide {
 public:
 	ListNode* listDivide(ListNode* head, int val) {
 		// write code here
+		ListNode *pre = new ListNode(0);
+		ListNode *mid = new ListNode(0);
+		ListNode *rear = new ListNode(0);
+		ListNode *pre_cu = pre;
+		ListNode *mid_cu = mid;
+		ListNode *rear_cu = rear;
+		ListNode *current = head;
+		while (current != NULL)
+		{
+			if (current->val < val)
+			{
+				pre_cu->next = current;
+				pre_cu = pre_cu->next;
+				current = current->next;
+				pre_cu->next = NULL;
+			}
+			else if(current->val > val)
+			{
+				rear_cu->next = current;
+				rear_cu = rear_cu->next;
+				current = current->next;
+				rear_cu->next = NULL;
+			}
+			else
+			{
+				mid_cu->next = current;
+				mid_cu = mid_cu->next;
+				current = current->next;
+				mid_cu->next = NULL;
+			}
+		}
+
+		return pre_cu;
 	}
 };
 
 int main()
 {
+	ListNode node1(1);
+	ListNode node2(4);
+	ListNode node3(2);
+	ListNode node4(5);
+	node1.next = &node2;
+	node2.next = &node3;
+	node3.next = &node4;
+	Divide d;
+	ListNode *result;
+	result = d.listDivide(&node1, 3);
 	return 0;
 }
 
