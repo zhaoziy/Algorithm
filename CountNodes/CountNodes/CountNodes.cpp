a7fb960b3cd6140865877bab1c1120f3ab05859b
Ziyun Zhao <zhaoziy@hotmail.com>
2016-01-18 12:59:24 +0800

    new

---
+++ CountNodes/CountNodes/CountNodes.cpp
@@ -0,0 +1,27 @@
+// CountNodes.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+		val(x), left(NULL), right(NULL) {
+	}
+};
+
+class CountNodes {
+public:
+	int count(TreeNode* root) {
+		// write code here
+	}
+};
+
+int main()
+{
+	return 0;
+}
+
