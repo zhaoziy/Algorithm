dcf727df0919141199d798542c8d989ae53c1f22
Ziyun Zhao <zhaoziy@hotmail.com>
2016-01-18 19:03:43 +0800

    new

--- CountNodes/CountNodes/CountNodes.cpp
+++ CountNodes/CountNodes/CountNodes.cpp
@@ -2,7 +2,7 @@
 //
 
 #include "stdafx.h"
-
+#include <math.h>
 
 struct TreeNode {
 	int val;
@@ -17,7 +17,11 @@ class CountNodes {
 public:
 	int count(TreeNode* root) {
 		// write code here
-		int Icount= 1;
+		if (root == NULL)
+			return 0;
+		if (root->left == NULL && root->right == NULL)
+			return 1;
+		int Icount = 0;
 		TreeNode *leftNode = root;
 		while (leftNode != NULL)
 		{
@@ -26,30 +30,39 @@ public:
 		}
 		int i = 1;
 		leftNode = root;
-		while (leftNode != NULL)
+		while (leftNode != NULL && leftNode->left != NULL)
 		{
-			if (i++ == Icount)
-			{
-				leftNode = leftNode->left;
-			}
-			else
-			{
-				leftNode = leftNode->right;
-			}
+			leftNode = (i++ == 1) ? (leftNode->right) : (leftNode->left);
 		}
-		if (leftNode != NULL)
-		{
-			//×ó×ÓÊ÷ÎªÂúÊ÷
-		}
-		else
-		{
-			//ÓÒ×ÓÊ÷ÎªÂúÊ÷
-		}
+		return (i == Icount) ? (pow(2, i - 1) - 1 + count(root->right) + 1) : (pow(2, i - 1) - 1 + count(root->left) + 1);
 	}
 };
 
 int main()
 {
+	TreeNode* node1 = new TreeNode(1);
+	TreeNode* node2 = new TreeNode(2);
+	TreeNode* node3 = new TreeNode(3);
+	TreeNode* node4 = new TreeNode(4);
+	TreeNode* node5 = new TreeNode(5);
+	TreeNode* node6 = new TreeNode(6);
+	/*TreeNode* node7 = new TreeNode(7);
+	TreeNode* node8 = new TreeNode(8);
+	TreeNode* node9 = new TreeNode(9);*/
+	node1->left = node2;
+	node1->right = node3;
+
+	node2->left = node4;
+	node2->right = node5;
+
+	node3->left = node6;
+	/*node3->right = node7;
+
+	node4->left = node8;
+	node4->right = node9;*/
+
+	CountNodes c;
+	int a = c.count(node1);
 	return 0;
 }
 
---
+++ QuickPower/QuickPower/QuickPower.cpp
@@ -0,0 +1,20 @@
+// QuickPower.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+class QuickPower {
+public:
+	int getPower(int k, int N) {
+		// write code here
+
+	}
+};
+
+int main()
+{
+	QuickPower q;
+	int a = q.getPower(2, 3);
+    return 0;
+}
+
