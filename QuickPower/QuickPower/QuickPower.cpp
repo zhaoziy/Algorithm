74ed633c09de7d9b2341c1e8aadfb975a96c206f
Ziyun Zhao <zhaoziy@hotmail.com>
2016-01-19 00:07:24 +0800

    new

--- QuickPower/QuickPower/QuickPower.cpp
+++ QuickPower/QuickPower/QuickPower.cpp
@@ -7,14 +7,29 @@ class QuickPower {
 public:
 	int getPower(int k, int N) {
 		// write code here
-
+		if (N == 0)
+			return 1;
+		if (N == 1)
+			return k;
+		long long temp = k;
+		long long res = 1;
+		while (N)
+		{
+			if (N & 1)
+			{
+				res = res * temp % 1000000007;
+			}
+			temp = temp * temp % 1000000007;
+			N = N >> 1;
+		}
+		return res;
 	}
 };
 
 int main()
 {
 	QuickPower q;
-	int a = q.getPower(2, 3);
+	int a = q.getPower(2, 14876069);
     return 0;
 }
 
