bed3e525d2478271b354bf24db2d8852c502643c
Ziyun Zhao <zhaoziy@hotmail.com>
2016-01-20 18:00:27 +0800

    new

--- LongestDistance/LongestDistance/LongestDistance.cpp
+++ LongestDistance/LongestDistance/LongestDistance.cpp
@@ -16,8 +16,35 @@ struct TreeNode {
 
 class LongestDistance {
 public:
+	void get(TreeNode *rt, int *mx, int &ret) 
+	{
+		int lmx = 0, rmx = 0;
+		if (rt->left) 
+		{
+			get(rt->left, mx, ret); 
+			lmx = mx[rt->left->val];
+		}
+		if (rt->right)
+		{
+			get(rt->right, mx, ret); 
+			rmx = mx[rt->right->val];
+		}
+		mx[rt->val] = max(lmx, rmx) + 1;
+		if (lmx + rmx + 1 > ret) 
+			ret = lmx + rmx + 1;
+	}
+
 	int findLongest(TreeNode* root) {
 		// write code here
+		int ret = 0;
+		int mx[550];
+		get(root, mx, ret);
+		return ret;
+	}
+
+	int max(int a, int b)
+	{
+		return (a > b) ? (a) : (b);
 	}
 };
 
