d67cb9fbb7edb47946d6c282ecd0904f728960b4
Ziyun Zhao <zhaoziy@hotmail.com>
2016-02-05 16:48:37 +0800

    new

---
+++ FindPath/FindPath/FindPath.cpp
@@ -0,0 +1,28 @@
+// FindPath.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+		val(x), left(NULL), right(NULL) {
+	}
+};
+
+class Solution {
+public:
+	vector<vector<int> > FindPath(TreeNode* root, int expectNumber) {
+
+	}
+};
+
+int main()
+{
+	return 0;
+}
+
--- VerifySquenceOfBST/VerifySquenceOfBST/VerifySquenceOfBST.cpp
+++ VerifySquenceOfBST/VerifySquenceOfBST/VerifySquenceOfBST.cpp
@@ -8,12 +8,91 @@ using namespace std;
 class Solution {
 public:
 	bool VerifySquenceOfBST(vector<int> sequence) {
+		if (sequence.size() == 0)
+			return false;
+		return judge(sequence, 0, sequence.size() - 1);
+	}
 
+	bool judge(vector<int> &sequence, int left, int right)
+	{
+		if (sequence.size() == 0 || left >= right)
+			return true;
+
+		int i = 0;
+		for (i = right; i >= left; --i)
+		{
+			if (sequence[i] < sequence[right])
+			{
+				break;
+			}
+		}
+		for (int j = i; j >= left; --j)
+		{
+			if (sequence[j] > sequence[right])
+			{
+				return false;
+			}
+		}
+		return judge(sequence, left, i) && judge(sequence, i + 1, right - 1);
 	}
 };
 
+//class Solution {
+//public:
+//	bool VerifySquenceOfBST(vector<int> sequence) {
+//		if (sequence.size() == 0)
+//			return false;
+//		return judge(sequence);
+//	}
+//
+//	bool judge(vector<int> sequence)
+//	{
+//		if (sequence.size() == 0)
+//			return true;
+//		int root = sequence[sequence.size() - 1];
+//		bool ret = true;
+//		vector<int> leftVec;
+//		vector<int> rightVec;
+//		int i = 0;
+//		for (i = 0; i < sequence.size() - 1; ++i)
+//		{
+//			if (sequence[i] < root)
+//			{
+//				leftVec.push_back(sequence[i]);
+//			}
+//			else
+//			{
+//				break;
+//			}
+//		}
+//		for (; i < sequence.size() - 1; ++i)
+//		{
+//			if (sequence[i] < root)
+//			{
+//				return false;
+//			}
+//			else
+//			{
+//				rightVec.push_back(sequence[i]);
+//			}
+//		}
+//		return judge(leftVec) && judge(rightVec);
+//	}
+//};
+
 int main()
 {
+	vector<int> leftVec;
+	leftVec.push_back(1);
+	leftVec.push_back(3);
+	leftVec.push_back(2);
+	leftVec.push_back(5);
+	leftVec.push_back(7);
+	leftVec.push_back(6);
+	leftVec.push_back(4);
+	Solution s;
+	s.VerifySquenceOfBST(leftVec);
+
     return 0;
 }
 
