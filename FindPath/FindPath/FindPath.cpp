24d0947706873729d08e88935db623e10ac766bc
Ziyun Zhao <zhaoziy@hotmail.com>
2016-02-08 10:08:39 +0800

    NEW

--- FindPath/FindPath/FindPath.cpp
+++ FindPath/FindPath/FindPath.cpp
@@ -23,6 +23,23 @@ public:
 
 int main()
 {
+	TreeNode Node1(1);
+	TreeNode Node2(2);
+	TreeNode Node3(3);
+	TreeNode Node4(4);
+	TreeNode Node5(5);
+	TreeNode Node6(6);
+	TreeNode Node7(7);
+	Node1.left = &Node2;
+	Node1.right = &Node3;
+	Node2.left = &Node4;
+	Node2.right = &Node5;
+	Node3.left = &Node6;
+	Node3.right = &Node7;
+
+	Solution dfs;
+	vector<vector<int>> Result;
+	Result = dfs.FindPath(&Node1,4);
 	return 0;
 }
 
