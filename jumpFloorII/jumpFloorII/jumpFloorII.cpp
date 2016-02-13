d7189e0f195784983933db27b83b3b860d569ab0
Ziyun Zhao <zhaoziy@hotmail.com>
2016-02-13 20:31:35 +0800

    new

--- jumpFloorII/jumpFloorII/jumpFloorII.cpp
+++ jumpFloorII/jumpFloorII/jumpFloorII.cpp
@@ -6,12 +6,16 @@
 class Solution {
 public:
 	int jumpFloorII(int number) {
-
+		if (number == 1)
+			return 1;
+		return 2 * jumpFloorII(number - 1);
 	}
 };
 
 int main()
 {
+	Solution s;
+	int i = s.jumpFloorII(2);
     return 0;
 }
 
