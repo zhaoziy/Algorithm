c30c541beabbcf1f6636267f02f508cd70b3b290
Ziyun Zhao <zhaoziy@hotmail.com>
2016-04-10 14:35:31 +0800

    new

--- 204B/204B/204B.cpp
+++ 204B/204B/204B.cpp
@@ -22,8 +22,8 @@ typedef struct Card
 };
 
 InitCard initcards[N];
-Card cards[N];
-int A[N];
+Card cards_Front[N];
+Card cards_Back[N];
 
 int comp(const void*a, const void*b)
 {
@@ -31,137 +31,149 @@ int comp(const void*a, const void*b)
 	struct Card*d = (Card*)b;
 	return d->num - c->num;
 }
+int comp1(const void*a, const void*b)
+{
+	struct InitCard*c = (InitCard*)a;
+	struct InitCard*d = (InitCard*)b;
+	return c->frontCard - d->frontCard;
+}
+int comp2(const void*a, const void*b)
+{
+	struct InitCard*c = (InitCard*)a;
+	struct InitCard*d = (InitCard*)b;
+	return c->backCard - d->backCard;
+}
 
 int main()
 {
 	int Count = 0;
 	while (scanf("%d", &Count) != EOF)
 	{
-		for (int i = 0; i < N; ++i)
-		{
-			initcards[i].backCard = -1;
-			initcards[i].frontCard = -1;
-			cards[i].Color = -1;
-			cards[i].num = 0;
-			A[i] = -1;
-		}
 		for (int i = 0; i < Count; ++i)
 		{
 			int a = 0, b = 0;
-			scanf("%d %d", &a, &b);
-			initcards[i].frontCard = a;
-			A[i] = a;
-			initcards[i].backCard = b;
+			scanf("%d %d", &initcards[i].frontCard, &initcards[i].backCard);
 		}
-		int num = 0;
-		for (int i = 0; i < Count; ++i)
+		int num_Front = 0;
+		qsort(initcards, Count, sizeof(InitCard), comp1);
+		int i = 0;
+		while (i < Count)
 		{
-			if (A[i] != -1)
+			cards_Front[num_Front].Color = initcards[i].frontCard;
+			int begin = i, end = Count - 1;
+			int mid = 0;
+			int right = 0;
+			while (begin < end)
 			{
-				cards[num].Color = A[i];
-				for (int j = i; j < Count; ++j)
-				{
-					if (A[j] == cards[num].Color)
-					{
-						cards[num].num++;
-						A[j] = -1;
-					}
-				}
-				num++;
+				mid = begin + (end - begin) / 2;
+				if (initcards[mid].frontCard > cards_Front[num_Front].Color)
+					end = mid - 1;
+				else
+					begin = mid + 1;
 			}
-		}
-		qsort(cards, num, sizeof(Card), comp);
-		bool flag = false;
-		for (int i = 0; i < num; ++i)
-		{
-			int ret_count = cards[i].num;
-			if (Count % 2 == 0)
+			if (begin == end)
 			{
-				if (ret_count >= Count / 2)
-				{
-					printf("0\n");
-					flag = true;
-					break;
-				}
+				if (initcards[begin].frontCard == cards_Front[num_Front].Color)
+					right = begin;
+				else
+					right = begin - 1;
 			}
 			else
+				right = end;
+			cards_Front[num_Front].num = right - i + 1;
+			i = ++right;
+			num_Front++;
+		}
+		qsort(cards_Front, num_Front, sizeof(Card), comp);
+
+		int num_Back = 0;
+		qsort(initcards, Count, sizeof(InitCard), comp2);
+		i = 0;
+		while (i < Count)
+		{
+			cards_Back[num_Back].Color = initcards[i].backCard;
+			int begin = i, end = Count - 1;
+			int mid = 0;
+			int right = 0;
+			while (begin < end)
 			{
-				if (ret_count > Count / 2)
-				{
-					printf("0\n");
-					flag = true;
-					break;
-				}
+				mid = begin + (end - begin) / 2;
+				if (initcards[mid].backCard > cards_Back[num_Back].Color)
+					end = mid - 1;
+				else
+					begin = mid + 1;
 			}
-			
-			for (int j = 0; j < Count; ++j)
+			if (begin == end)
 			{
-				if(initcards[j].frontCard == cards[i].Color)
-					continue;
+				if (initcards[begin].backCard == cards_Back[num_Back].Color)
+					right = begin;
 				else
+					right = begin - 1;
+			}
+			else
+				right = end;
+			cards_Back[num_Back].num = right - i + 1;
+			i = ++right;
+			num_Back++;
+		}
+		qsort(cards_Back, num_Back, sizeof(Card), comp);
+
+		if (Count % 2 == 0 && cards_Front[0].num >= Count / 2)
+		{
+			printf("0");
+			break;
+		}
+		else if (Count % 2 == 1 && cards_Front[0].num > Count / 2)
+		{
+			printf("0");
+			break;
+		}
+
+		bool flag = false;
+		for (int i = 0; i < num_Front; ++i)
+		{
+			for (int j = 0; j < num_Back; ++j)
+			{
+				if (cards_Back[j].Color == cards_Front[i].Color)
 				{
-					if (initcards[j].backCard == cards[i].Color)
-						ret_count++;
-				}
-				if (Count % 2 == 0)
-				{
-					if (ret_count >= Count / 2)
+					if (Count % 2 == 0)
 					{
-						printf("%d\n", ret_count - cards[i].num);
-						flag = true;
-						break;
+						if (cards_Back[j].num + cards_Front[i].num >= Count / 2)
+						{
+							printf("%d\n", cards_Back[j].num - cards_Front[i].num);
+							flag = true;
+							break;
+						}
 					}
-				}
-				else
-				{
-					if (ret_count > Count / 2)
+					else
 					{
-						printf("%d\n", ret_count - cards[i].num);
-						flag = true;
-						break;
+						if (cards_Back[j].num + cards_Front[i].num > Count / 2)
+						{
+							printf("%d\n", cards_Back[j].num - cards_Front[i].num + 1);
+							flag = true;
+							break;
+						}
 					}
 				}
 			}
 			if (flag == true)
 				break;
 		}
-		if (flag == false)
+		if (flag == true)
 		{
-			int ret = initcards[0].backCard;
-			int ret_num = 1;
-			for (int i = 1; i < Count; ++i)
+			if (Count % 2 == 0 && cards_Front[i].num >= Count / 2)
 			{
-				if (initcards[i].backCard == ret)
-					ret_num++;
-				else
-					ret_num--;
-				if (ret_num < 0)
-				{
-					ret = initcards[i].backCard;
-					ret_num = 1;
-				}
+				printf("%d\n", Count / 2);
+				break;
 			}
-			ret_num = 0;
-			for (int i = 0; i < Count; ++i)
+			else if (Count % 2 == 1 && cards_Front[i].num > Count / 2)
 			{
-				if (initcards[i].backCard == ret)
-					ret_num++;
+				printf("%d\n", Count / 2 + 1);
+				break;
 			}
-			if (Count % 2 == 0)
-			{
-				if (ret_num >= Count / 2)
-					printf("%d", ret_num);
-				else
-					printf("-1\n");
-			}
-			else
-			{
-				if (ret_num > Count / 2)
-					printf("%d", ret_num);
-				else
-					printf("-1\n");
-			}
-		}	
+		}
+		else
+			printf("-1");
 	}
 	return 0;
 }
